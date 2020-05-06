fbf76a35c9cea06b028dd8bd88a5329b3fcfeac3
blk-hammer <rishabhagnihotri1999@gmail.com>
2020-05-06 20:53:20 +0530

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_qAphjeY5EoS7T5KMBXFmS8ep00cQTCK73o');
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
